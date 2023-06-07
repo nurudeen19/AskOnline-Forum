5f2e4f0fc31c36e4332490a6cd1753f85bc6ee62
nurudeen <nurudeenh@gmail.com>
2023-06-07 15:42:07 +0000

    create Category, Posts and Comments models with migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.text import slugify
+from profiles.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=255, unique=True)
+	slug = models.SlugField()
+
+
+	#auto add slug field
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+
+	def __str__(self):
+		return self.name
+
+
+
+class Post(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
+	title = models.CharField(max_length=255)
+	slug = models.SlugField()
+	description = models.TextField()
+
+
+	#auto add slug field
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Post, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+	description = models.TextField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+	post = models.ForeignKey(Post, on_delete= models.CASCADE, default=None)
+	parent= models.ForeignKey("Comment", on_delete=models.CASCADE, null=True)
